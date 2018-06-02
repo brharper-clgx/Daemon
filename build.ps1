c11e398b79bed6986784edfbeec8b3b3b3baf14e
brharper-clgx <bryanbharper@gmail.com>
2018-06-02 11:56:36 -0500

    test

---
+++ build.ps1
@@ -0,0 +1,39 @@
+param (
+    [string]$task = ""
+)
+
+
+If ($task -eq "") {
+    Write-Output "you must choose a task"
+	Write-Output "---------------------"
+	Write-Output "push"
+    [Environment]::Exit(1)
+}
+
+function Start-Job-Here([scriptblock]$block){
+	Start-Job -Name dotnet -ArgumentList (Get-Location),$block { Set-Location $args[0]; Invoke-Expression $args[1] }
+}
+
+function Test-Error {
+    If (-Not $?) {
+		try {
+			Stop-Process -Name dotnet 2>$null
+		}
+		catch {
+
+		}
+		# this should never execute
+        throw "How can this happened"
+    }
+}
+
+If ($task -eq "push") {
+	git pull -r
+	Test-Error
+	mdpdf backstory.md
+	Test-Error
+	git push
+	exit
+}
+
+Invoke-Expression $task
